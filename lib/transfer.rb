b09684db7ec64befc1f6b1e0dbe7c7837d9ef6d0
Jake <jakelovitz@gmail.com>
2019-04-25 11:32:54 -0400

    solved OO-Banking with @yashartung

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,42 @@
+require 'Pry'
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  def initialize (sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    if sender.valid? && receiver.valid?
+      return true
+    else
+      return false
+    end
+  end
+
+  def execute_transaction
+    if sender.valid? == true && sender.balance - @amount > 0
+      if @status == "pending"
+
+        receiver.deposit(@amount)
+        sender.deposit(-@amount)
+        @status = "complete"
+      end
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      sender.deposit(@amount)
+      receiver.deposit(-@amount)
+      @status = "reversed"
+    end
+  end
+
 end
